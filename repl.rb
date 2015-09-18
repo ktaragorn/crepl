444885d7e1be8c4325a44445e2fd701ee8b65deb
Karthik T <karthikt.holmes+github@gmail.com>
2015-09-18 11:53:11 +0800

    Rework the repl to be a repl for crystal
    It works only 1 line at a time, no state is stored
    multiple lines can be submitted by delimitting with ;
    Writes the input into a file, and execute tat file with crystal <file>

--- repl.rb
+++ repl.rb
@@ -1,11 +1,22 @@
-puts "Ruby REPL"
+#!env ruby
+puts "Crystal REPL"
+require "pry"
+def crystal_eval(crystal)
+  File.write("crystal_code.cr", "puts #{crystal}")
+  require "open3"
+  Open3.popen3("crystal crystal_code.cr") do |stdin, stdout, stderr, wait_thr|
+    err = stderr.read
+    raise stderr.read unless err.empty?
+    return stdout.read
+  end
+end
 
 # Handle the input, this would probably run some method
 # as a part of the DSL you'd have to create. Place this
 # repl as your command line interface to your service.
 def handle_input(input)
-  result = eval(input)
-  puts(" => #{result}")
+  result = crystal_eval(input)
+  puts("=> #{result}")
 end
 
 # This is a lambda that runs the content of the block
